9a14c016a9f96eaac7b9c5292e8ec15506b946fb
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-03-18 16:53:38 +0300

    add my files

---
+++ src/main.kt
@@ -0,0 +1,40 @@
+fun main(){
+     var numbers = multiply(arrayOf(4,5,6,10))
+     println(numbers)
+     var nums = sumMixedArray(arrayOf(30.5F, 3, 40.5))
+     println(nums)
+     var vowels = noVowelsArray(arrayOf('p','a','w', 'e', 'o', 'q' ))
+     println(vowels)
+}
+//Write a function that takes in an array of integers and returns the product of
+//all the elements
+fun multiply(nums:Array<Int>):Int{
+     var product = 1
+     nums.forEach { nums ->
+          product *= nums
+     }
+     return product
+}
+//Write a function that takes in an array of mixed types and returns the sum of
+//all the decimal elements
+fun sumMixedArray(number: Array<Any>):Double{
+     var sum = 0.0
+     number.forEach { number ->
+          sum+=number.toString().toDouble()
+     }
+     return sum
+}
+//Write a function that takes in an array of characters and returns the number of
+//vowels in the array
+fun noVowelsArray(vowels: Array<Char>):Int {
+     var letters = 0
+     vowels.forEach { char ->
+          if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
+               ++letters
+          }
+     }
+     return letters
+}
+
+
+
